b04fa370196272a09fa460af5adada1e9330105e
Vaishnavi-D-R <vaishnavidr200@gmail.com>
2021-02-04 17:23:45 +0530

    my commit from eclipse

--- git-challenge4/src/Merging.java
+++ git-challenge4/src/Merging.java
@@ -3,8 +3,8 @@ public class Merging {
 public static void main(String []args) {
 	System.out.println("Hello");
 	
-	int x=10;
-	int y=10;
-	System.out.println(x+y);
+	int data1=10;
+	int data2=10;
+	System.out.println(data1+data2);
 }
 }
