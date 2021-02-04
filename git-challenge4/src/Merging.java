b0458289258df222f5d92a35146c4bd32794b3f2
Vaishnavi-D-R <78353427+Vaishnavi-D-R@users.noreply.github.com>
2021-02-04 17:22:34 +0530

    Added multiplication operation

--- git-challenge4/src/Merging.java
+++ git-challenge4/src/Merging.java
@@ -3,8 +3,8 @@ public class Merging {
 public static void main(String []args) {
 	System.out.println("Hello");
 	
-	int x=10;
-	int y=10;
-	System.out.println(x+y);
+	int m1=10;
+	int m2=10;
+	System.out.println(m1*m2);
 }
 }
