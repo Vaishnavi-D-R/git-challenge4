c530bb43519c706ddbc3b00d6a5a8a9bb9fd5b06
Vaishnavi-D-R <78353427+Vaishnavi-D-R@users.noreply.github.com>
2021-02-05 09:30:01 +0530

    Added division operation

--- git-challenge4/src/Merging.java
+++ git-challenge4/src/Merging.java
@@ -3,9 +3,9 @@ public class Merging {
 public static void main(String []args) {
 	System.out.println("Hello");
 	
-	int arg1=10;
-	int arg2=10;
-	System.out.println(arg1+arg2);
+	int i1=5;
+	int i2=15;
+	System.out.println(i1/i2);
 
 }
 }
