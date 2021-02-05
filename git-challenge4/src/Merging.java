1d276484b5b54a44dbec344abab38fc560d7cb6e
Vaishnavi-D-R <78353427+Vaishnavi-D-R@users.noreply.github.com>
2021-02-05 09:23:24 +0530

    To check merge conflicts

--- git-challenge4/src/Merging.java
+++ git-challenge4/src/Merging.java
@@ -3,9 +3,9 @@ public class Merging {
 public static void main(String []args) {
 	System.out.println("Hello");
 	
-	int m1=10;
-	int m2=10;
-	System.out.println(m1*m2);
+	int arg1=10;
+	int arg2=10;
+	System.out.println(arg1+arg2);
 
 }
 }
